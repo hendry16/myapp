380bed6b6a53e9780a7708304b51d25f4796464c
Hendri Sibarani <hendrisibarani15@gmail.com>
2022-09-30 14:17:52 +0700

    begin without anything else

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Example from './Example';
 
 function App() {
   return (
      <div>
-	  {/* <h1 className="text-3xl font-bold underline">
-	  	Hello World!
-	  </h1> */}
-      <Example />
+
      </div>
   );
 }
